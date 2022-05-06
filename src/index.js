6dc3df3305267fff8c4f8ddc900fecf3f5686136
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-05-06 17:54:41 +0300

    correct result object for  all api request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,2 @@
 import './sass/main.scss';
 import { getPopularFilm, searchFilmByName, getFilmDescription } from './js/service/api';
-getPopularFilm();
-searchFilmByName('maestro');
-getFilmDescription(26);
