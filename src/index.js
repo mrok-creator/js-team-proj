993bd629c669462cc47e42db24e24343446e88c8
Andari757 <matviyskrypka@gmail.com>
2022-05-06 20:38:44 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
 import './sass/main.scss';
 import * as basicLightbox from 'basiclightbox'
 
-function get() {
-    fetch("https://api.themoviedb.org/3/movie/566574?api_key=453647fe51ddb15dbe812a48a21b448b&language=en-US")
+export function makeModal() {
+    fetch("https://api.themoviedb.org/3/movie/453395?api_key=453647fe51ddb15dbe812a48a21b448b&language=en-US")
         .then(r => { return r.json() })
         .then(r => {
            const markup = `
-    <div class="modal">          
+        <div class="modal">          
         <svg  class="modal__icon">
-          <use href="/sprite.2b07476a.svg#icon-close" ></use>
+            <use href="/sprite.2b07476a.svg#icon-close" ></use>
         </svg>      
-      <div class="modal__wrapper">
-      <img class="modal__img" src="https://image.tmdb.org/t/p/w500${r.poster_path}" alt="" />
-      <div class="modal__name">${r.title.toUpperCase()}</div>
-      <div class="modal__info">
+        <div class="modal__wrapper">
+        <img class="modal__img" src="https://image.tmdb.org/t/p/w500${r.poster_path}" alt="" />
+        <div class ="modal__data-wrapper">
+        <div class="modal__name">${r.title.toUpperCase()}</div>
+        <div class="modal__info">
         <ul class="modal__keys">
-          <li class="modal__key">Vote / Votes</li>
-          <li class="modal__key">Popularity</li>
-          <li class="modal__key">Original Title</li>
-          <li class="modal__key">Genre</li>
+            <li class="modal__key">Vote / Votes</li>
+            <li class="modal__value"><span>${r.vote_average}</span> / <span>${r.vote_count}</span></li>
+            <li class="modal__key">Popularity</li>
+            <li class="modal__value">${r.popularity}</li>
+            <li class="modal__key">Original Title</li>
+            <li class="modal__value">${r.original_title}</li>
+            <li class="modal__key">Genre</li>
+            <li class="modal__value">${r.genres.map(e =>e.name)}</li>
         </ul>
-        <ul class="modal__values">
-          <li class="modal__values"><span>${r.vote_average}</span> / ${r.vote_count}</li>
-          <li class="modal__values">${r.popularity}</li>
-          <li class="modal__values">${r.original_title}</li>
-          <li class="modal__values">${r.genres.map(e =>e.name)}</li>
-        </ul>
-      </div>
-      <div>ABOUT</div>
-      <p class="modal__descr">${r.overview}</p>      
-      <div class="modal__buttons"><button class="modal__button modal__button-watched" type="button">ADD TO WATCHED</button>
-      <button class="modal__button modal__button-q"type="button">ADD TO QUEUE</button></button> </div>   
-    </div>
-`
-            createBox(markup)
-            console.log(r)            
+        </div>
+        <div>ABOUT</div>
+        <p class="modal__descr">${r.overview}</p>      
+        <div class="modal__buttons"><button class="modal__button modal__button-watched" type="button">ADD TO WATCHED</button>
+        <button class="modal__button modal__button-q"type="button">ADD TO QUEUE</button></button> </div>  </div> 
+        </div>
+        </div>`
+            createBox(markup)                      
         })
 }
 
@@ -47,4 +45,4 @@ function createBox(markup) {
     instance.show()
 }
 
-get()
+makeModal()
