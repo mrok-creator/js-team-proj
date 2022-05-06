2074fe6967dc7ded7c6dd3bad4dfd81bb1122629
Andari757 <matviyskrypka@gmail.com>
2022-05-06 14:48:00 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import './sass/main.scss';
 
 import * as basicLightbox from 'basiclightbox'
-function get() {
-    fetch("https://api.themoviedb.org/3/movie/629542/images?api_key=453647fe51ddb15dbe812a48a21b448b&language=en-US")
-        .then(r => { return r.json() })
-        .then(r => console.log(r))
-}
-get()
+// function get() {
+//     fetch("https://api.themoviedb.org/3/movie/629542/images?api_key=453647fe51ddb15dbe812a48a21b448b&language=en-US")
+//         .then(r => { return r.json() })
+//         .then(r => console.log(r))
+// }
+// get()
 const instance = basicLightbox.create(`
-    <div class="modal">
-        
-      <button type="button">
-        <svg style="width: 30px; height: 30px" class="close-icon">
-          <use href="./sprite.svg#icon-close" style="width: 30px; stroke: black"></use>
-        </svg>
-      </button>
-      <img src="" alt="" />
-      <span></span>
+    <div class="modal">          
+        <svg  class="modal__icon">
+          <use href="/sprite.2b07476a.svg#icon-close" ></use>
+        </svg>      
+      <img class="modal__img" src="https://upload.wikimedia.org/wikipedia/ru/thumb/c/c7/Doctor_Strange_poster.jpg/640px-Doctor_Strange_poster.jpg" alt="" />
+      <span class=""></span>
       <div>
         <ul>
           <li></li>
@@ -32,15 +29,13 @@ const instance = basicLightbox.create(`
         </ul>
       </div>
       <span></span>
-      <span></span>
+      <p></p>
       <button type="button">add to watched</button>
-      <button type="button">add to queue</button>
-   
-        <a>Close</a>
+      <button type="button">add to queue</button>          
     </div>
 `, {
     onShow: (instance) => {
-        instance.element().querySelector('a').onclick = instance.close
+        instance.element().querySelector('svg').onclick = instance.close
     }
 })
 
