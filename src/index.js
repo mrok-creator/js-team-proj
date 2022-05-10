fa64d960920d743a777e50f6559e4200295e3fea
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-05-11 02:33:06 +0300

    add up to top button

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './sass/main.scss';
-import './js/properties/dark-mode.js';
+
 import { getPopularFilm, searchFilmByName, getFilmDescription } from './js/service/api';
 
 import {
@@ -8,7 +8,7 @@ import {
   pushData,
   getFromFirebase,
 } from './js/service/firebase-api';
-import Darkmode from 'darkmode-js';
+
 import { makeFilmModal } from './js/modal';
 import { debounceOnInputSearch, listenerForInput } from './js/searchFilm';
 
@@ -17,23 +17,6 @@ import { libraryBtn, loginBtn, logOutBtn } from './js/ref';
 import { loadPopular } from './js/popularMovie';
 
 import { onScroll, onUpBtnClick } from './js/properties/scroll';
-
-const darkmode = new Darkmode({
-  // the position of the toggle button
-  bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
-  mixColor: '#fff', // default: '#fff'
-  backgroundColor: '#fff', // default: '#fff'
-  buttonColorDark: '#054f05', // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
-  label: '🌓', // default: ''
-  autoMatchOsTheme: false,
-});
-darkmode.showWidget();
-darkmode.toggle();
 
 isUserSignIn();
 onScroll();
