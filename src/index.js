8fce47b1a7cc64bf3057facc5807f767f2b705ab
Kasianenko Oleksandr <sanekkas@gmail.com>
2022-05-12 10:16:05 +0300

    save theme after refresh

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const options = {
   backgroundColor: '#fff', // default: '#fff'
   buttonColorDark: '#100f2c', // default: '#100f2c'
   buttonColorLight: '#fff', // default: '#fff'
-  saveInCookies: false, // default: true,
+  saveInCookies: true, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true, // default: true
 };
